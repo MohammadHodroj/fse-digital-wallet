7dfd35884ba921a2897cd8065bd73005d1d8d6b7
Mohammad Hodroj <mohammad.hodroj.2001@gmail.com>
2025-02-28 19:55:21 +0200

    Added database.php

---
+++ wallet-server/connection/database.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/../connection/config.php';
+
+class Database {
+    private $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=".DB_HOST.";dbname=".DB_NAME,
+                DB_USER,
+                DB_PASS,
+                [
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+            );
+        } catch(PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+
+?>
