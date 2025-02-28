65d8f1534aeeea0839561be2d12eeee839b9bbbf
Mohammad Hodroj <mohammad.hodroj.2001@gmail.com>
2025-02-28 19:52:32 +0200

    Added config.php

---
+++ wallet-server/connection/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'digital_wallet_DB');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('BASE_URL', 'http://localhost/fse-digital-wallet/wallet-client');
+
+?>
